2482957aee910b8d1bf3681230779b9f990b0d7a
lalyos <lalyos@yahoo.com>
2016-01-17 18:36:01 +0100

    install cbd upto init

--- install-cbd.sh
+++ install-cbd.sh
@@ -2,6 +2,12 @@
 
 exec > >(tee "/tmp/${BASH_SOURCE}.log") 2>&1
 set -x
+
+yum install -y unzip
 pwd
-curl -Ls public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_${CBD_VERSION:=1.1.0}_$(uname)_x86_64.tgz | sudo tar -xz -C /bin cbd
-mkdir cloudbreak-deployment
+curl -Ls public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_${CBD_VERSION:=1.1.0}_$(uname)_x86_64.tgz | tar -xz -C /bin cbd
+
+mkdir /var/lib/cloudbreak-deployment
+cd $_
+echo export PUBLIC_IP=$(curl ipecho.net/plain) > Profile
+cbd init
