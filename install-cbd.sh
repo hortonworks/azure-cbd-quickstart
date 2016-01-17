2781ed7f685b24bc59e14c07287846235d4fab8d
lalyos <lalyos@yahoo.com>
2016-01-17 22:19:30 +0100

    add relocate_docker

--- install-cbd.sh
+++ install-cbd.sh
@@ -3,11 +3,26 @@
 exec > >(tee "/tmp/${BASH_SOURCE}.log") 2>&1
 set -x
 
-yum install -y unzip
-pwd
-curl -Ls public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_${CBD_VERSION:=1.1.0}_$(uname)_x86_64.tgz | tar -xz -C /bin cbd
+install_cbd() {
+    curl -Ls public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_${CBD_VERSION:=1.1.0}_$(uname)_x86_64.tgz | tar -xz -C /bin cbd
+    mkdir /var/lib/cloudbreak-deployment
+    cd $_
+    echo export PUBLIC_IP=$(curl ipecho.net/plain) > Profile
+    cbd init
+}
 
-mkdir /var/lib/cloudbreak-deployment
-cd $_
-echo export PUBLIC_IP=$(curl ipecho.net/plain) > Profile
-cbd init
+relocate_docker() {
+    service docker stop
+    rm -rf /var/lib/docker/
+    mkdir /mnt/resource/docker
+    ln -s /mnt/resource/docker /var/lib/docker
+    service docker start
+}
+
+main() {
+    yum install -y unzip
+    relocate_docker
+    install_cbd
+}
+
+[[ "$0" == "$BASH_SOURCE" ]] && main "$@" || true
