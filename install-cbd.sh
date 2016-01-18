bf103eee0e2cdd2daf1243afb7f5e2f5b1aa4dec
lalyos <lalyos@yahoo.com>
2016-01-18 08:49:01 +0100

    use user-data (azure's customData) from install script, as default waagent conf ignores it

--- install-cbd.sh
+++ install-cbd.sh
@@ -3,6 +3,23 @@
 exec > >(tee "/tmp/${BASH_SOURCE}.log") 2>&1
 set -x
 
+check_custom_data() {
+  CDATA_FILE=${CDATA_FILE}
+  if [[ -e $CDATA_FILE ]]; then
+    # if it hash shebang, run it
+    base64 -d $CDATA_FILE > ${CDATA_FILE}.txt
+    if head -1 ${CDATA_FILE}.txt|grep -qe '^#!' ;then
+        source ${CDATA_FILE}.txt
+    fi
+
+    # if it has CBD specific env settings
+    if grep -q CBD_ ${CDATA_FILE}.txt; then
+        eval "$(grep CBD_ ${CDATA_FILE}.txt)"
+    fi
+  fi
+
+}
+
 install_cbd() {
     curl -Ls public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_${CBD_VERSION:=1.1.0}_$(uname)_x86_64.tgz | tar -xz -C /bin cbd
     mkdir /var/lib/cloudbreak-deployment
@@ -22,6 +39,7 @@ relocate_docker() {
 main() {
     yum install -y unzip
     relocate_docker
+    check_custom_data
     install_cbd
 }
 
