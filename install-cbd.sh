76cc1885eac98a14c50ac84bfd8125cf6adb6773
lalyos <lalyos@yahoo.com>
2016-03-04 13:07:21 +0100

    fix failing external IP url

--- install-cbd.sh
+++ install-cbd.sh
@@ -24,7 +24,7 @@ install_cbd() {
     curl -Ls public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_${CBD_VERSION:=1.1.0}_$(uname)_x86_64.tgz | tar -xz -C /bin cbd
     mkdir /var/lib/cloudbreak-deployment
     cd $_
-    echo export PUBLIC_IP=$(curl ipecho.net/plain) > Profile
+    echo export PUBLIC_IP=$(curl ifconfig.co) > Profile
     cbd generate
 
     cbd pull-parallel
