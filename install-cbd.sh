c94063db4719b127b1f2de75065edfeb328abe8a
sodre90 <erdos.peter.bme@gmail.com>
2016-09-06 09:58:31 +0200

    env variables into Profile

--- install-cbd.sh
+++ install-cbd.sh
@@ -3,29 +3,17 @@
 exec > >(tee "/tmp/${BASH_SOURCE}.log") 2>&1
 set -x
 
-: ${CBD_VERSION:="1.3.0"}
+: ${CBD_VERSION:="snapshot"}
 : ${CBD_DIR:="/var/lib/cloudbreak-deployment"}
-: ${WAIT_FOR_CB_RETRY:=50}
 
-check_custom_data() {
-  CDATA_FILE=/var/lib/waagent/CustomData
-  if [[ -e $CDATA_FILE ]]; then
-    # if it hash shebang, run it
-    base64 -d $CDATA_FILE > ${CDATA_FILE}.txt
-    if head -1 ${CDATA_FILE}.txt|grep -qe '^#!' ;then
-        source ${CDATA_FILE}.txt
-    fi
-
-    # if it has CBD specific env settings
-    if grep -q CBD_ ${CDATA_FILE}.txt; then
-        eval "$(grep CBD_ ${CDATA_FILE}.txt)"
-    fi
-  fi
-
+custom_data() {
+    set -o allexport
+    source /tmp/.cbdprofile
+    set +o allexport
 }
 
 install_cbd() {
-    set +x
+    set -x
     curl -Ls s3.amazonaws.com/public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_${CBD_VERSION}_$(uname)_x86_64.tgz | tar -xz -C /bin cbd
     mkdir $CBD_DIR
     cd $_
@@ -33,23 +21,28 @@ install_cbd() {
     CREDENTIAL_NAME=defaultcredential
 
     echo export PUBLIC_IP=$(curl ifconfig.co) > Profile
-    echo "export AZURE_RESOURCE_GROUP_ID=$AZURE_RESOURCE_GROUP_ID" >> Profile
-    echo "export AZURE_VIRTUAL_NETWORK_ID=$AZURE_VIRTUAL_NETWORK_ID" >> Profile
-    echo "export AZURE_SUBNET_ID=$AZURE_SUBNET_ID" >> Profile
-    echo "export AZURE_DEFAULT_CREDENTIAL=$CREDENTIAL_NAME" >> Profile
+    echo "export ULU_HWX_CLOUD_PROVIDER='AZURE_RM'" >> Profile
+    echo "export ULU_HWX_CLOUD_DEFAULT_REGION='$ULU_HWX_CLOUD_DEFAULT_REGION'" >> Profile
+    echo "export ULU_HWX_CLOUD_DEFAULT_SUBNET_ID=$ULU_HWX_CLOUD_DEFAULT_SUBNET_ID" >> Profile
+    echo "export ULU_HWX_CLOUD_DEFAULT_CREDENTIAL=$CREDENTIAL_NAME" >> Profile
+    echo "export ULU_HWX_CLOUD_AZURE_DEFAULT_VIRTUAL_NETWORK_ID=$ULU_HWX_CLOUD_AZURE_DEFAULT_VIRTUAL_NETWORK_ID" >> Profile
+    echo "export ULU_HWX_CLOUD_AZURE_RESOURCE_GROUP=$ULU_HWX_CLOUD_AZURE_RESOURCE_GROUP" >> Profile
+    echo "export ULU_HWX_CLOUD_AZURE_SSH_KEY='$ULU_HWX_CLOUD_AZURE_SSH_KEY'" >> Profile
+    echo "export AZURE_TENANT_ID=$AZURE_TENANT_ID" >> Profile
+    echo "export AZURE_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID" >> Profile 
+    echo "export ULUWATU_CONTAINER_PATH=/hortonworks-cloud-web" >> Profile
+    echo "export DOCKER_IMAGE_CLOUDBREAK_WEB=hortonworks/cloud-web" >> Profile
+    echo "export DOCKER_TAG_ULUWATU=azure_aws" >> Profile
+    echo "export DOCKER_TAG_SULTANS=latest" >> Profile
+    echo "export DOCKER_IMAGE_CLOUDBREAK_AUTH=hortonworks/cloud-auth" >> Profile
+    echo "export CB_BLUEPRINT_DEFAULTS='EDW-ETL: Apache Hive 1.2.1, Apache Spark 1.6=hdp-etl-edw;Data Science: Apache Spark 1.6, Zeppelin=hdp25-data-science;25EDW-ETL: Apache Hive 1.2.1, Apache Spark 1.6=hdp25-etl-edw;EDW-ETL: Apache Spark 2.0-preview=hdp25-etl-edw-spark2;EDW-Analytics: Apache Hive 2 LLAP, Apache Zeppelin=hdp25-edw-analytics'" >> Profile
 
+    cbd version
+    cbd update azure_env_variables
+    cbd version
     cbd generate
     cbd pull-parallel
     cbd start-wait
-    echo "credential create --AZURE --name $CREDENTIAL_NAME --sshKeyString '$SSH_KEY_STRING' --subscriptionId $AZURE_SUBSCRIPTION_ID --tenantId $AZURE_TENANT_ID --appId $AZURE_APP_ID --password $AZURE_PASSWORD" >> create-default-azure-role.sh
-    cbd util cloudbreak-shell-quiet < create-default-azure-role.sh
-}
-
-execute_cb_shell_script() {
-    if [[ -n "$SHELL_SCRIPT" ]]; then
-        cd $CBD_DIR
-        echo "$(base64 -d <(echo "$SHELL_SCRIPT"))" | cbd util cloudbreak-shell-quiet
-    fi
 }
 
 relocate_docker() {
@@ -62,10 +55,9 @@ relocate_docker() {
 
 main() {
     apt-get install -y unzip
-    check_custom_data
+    custom_data
     #relocate_docker
     install_cbd
-    execute_cb_shell_script
 }
 
 [[ "$0" == "$BASH_SOURCE" ]] && main "$@" || true
