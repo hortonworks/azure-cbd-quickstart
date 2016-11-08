d0380a02a775af5b01190830363a502191453ed1
sodre90 <erdos.peter.bme@gmail.com>
2016-11-08 13:40:10 +0100

    use cbd image from marketplace

--- install-cbd.sh
+++ install-cbd.sh
@@ -14,7 +14,7 @@ custom_data() {
 
 install_cbd() {
     set -x
-    curl -Ls s3.amazonaws.com/public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_${CBD_VERSION}_$(uname)_x86_64.tgz | tar -xz -C /bin cbd
+    # curl -Ls s3.amazonaws.com/public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_${CBD_VERSION}_$(uname)_x86_64.tgz | tar -xz -C /bin cbd
     mkdir $CBD_DIR
     cd $_
 
