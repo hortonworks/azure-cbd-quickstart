58153e374e9273a2828147371f8f756afa0259fe
sodre90 <erdos.peter.bme@gmail.com>
2017-05-25 15:55:22 +0200

    generate 2 type of template

--- create-template.sh
+++ create-template.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-: ${IMAGE_NAME:?"need to set IMAGE_NAME"}
 : ${ARM_USERNAME:?"need to set ARM_USERNAME"}
 : ${ARM_PASSWORD:?"need to set ARM_PASSWORD"}
+: ${NEW_VERSION:?"need to set NEW_VERSION"}
 
-export IMAGE_VHD="$(docker run -it --rm azuresdk/azure-cli-python:0.2.9 az login --username $ARM_USERNAME --password $ARM_PASSWORD &> /dev/null; \
+echo "NEW_VERSION: $NEW_VERSION"
+
+IMAGE_NAME="$(atlas -s sequenceiq/cbd/azure-arm.image --meta cbd_version=$NEW_VERSION -l | jq .metadata.short_image_name -r)"
+
+echo "IMAGE_NAME: $IMAGE_NAME"
+
+IMAGE_VHD="$(docker run -it --rm azuresdk/azure-cli-python:0.2.9 az login --username $ARM_USERNAME --password $ARM_PASSWORD &> /dev/null; \
 az storage blob list -c system --account-name sequenceiqnortheurope2 --prefix Microsoft.Compute/Images/packer/$IMAGE_NAME | jq '.[0].name' -r | awk '{print $1;}')"
 
-sigil -f mainTemplate.tmpl VERSION="$NEW_VERSION" IMAGE_VHD="$IMAGE_VHD" > mainTemplate.json
+echo "IMAGE_VHD: $IMAGE_VHD"
+echo "OS_IMAGE_SKU_VERSION: $OS_IMAGE_SKU_VERSION"
+
+sigil -f mainTemplate.tmpl VERSION="$NEW_VERSION" IMAGE_VHD="$IMAGE_VHD" OS_IMAGE_SKU_VERSION="$OS_IMAGE_SKU_VERSION" > mainTemplate.json;
